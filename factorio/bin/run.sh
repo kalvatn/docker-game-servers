cb255aa12fb892ee9fe8214e7385e36b92185809
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2018-07-25 22:41:59 +0200

    working docker configuration

--- factorio/bin/run.sh
+++ factorio/bin/run.sh
@@ -3,9 +3,16 @@
 set -e
 # sudo mkdir -p /mnt/factorio
 # sudo chown 845:845 /mnt/factorio
+RCON_PORT=27015
+RCON_PASSWORD=""
+ADMIN_USERS=''
+GAME_PASSWORD=""
 
 docker run --rm -it \
   -p 34197:34197/udp \
-  -p 34198:34198/tcp \
+  -p $RCON_PORT:$RCON_PORT/tcp \
+  -e RCON_PASSWORD="$RCON_PASSWORD" \
+  -e CFG_ADMIN_USERS="$ADMIN_USERS" \
+  -e CFG_GAME_PASSWORD="$GAME_PASSWORD" \
   -v /mnt/factorio:/factorio \
-  dtandersen/factorio
+  factorio-headless:0.16
